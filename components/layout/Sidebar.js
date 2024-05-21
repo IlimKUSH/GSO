da5aea0760658512266d678ace295064178a0ee9
malchikprostoy <malchikprostoy5@gmail.com>
2024-05-21 17:57:45 +0600

    change some function on MobileMenu, Sidebar, Footer and text color

--- components/layout/Sidebar.js
+++ components/layout/Sidebar.js
@@ -2,7 +2,7 @@ import Link from "next/link"
 import { useState } from "react"
 import {useTranslations} from "next-intl";
 
-export default function Sidebar() {
+export default function Sidebar({handleLinkClick}) {
     const t = useTranslations()
 
     const [isActive, setIsActive] = useState({
@@ -26,49 +26,49 @@ export default function Sidebar() {
         <>
             <ul className="navbar_nav getmobilemenu">
                 <li className="menu-item nav-item menu-item-has-children dropdown mennucolumn_full-six">
-                    <Link href="#" className="nav_link">
+                    <Link href="#" className="nav_link" onClick={handleLinkClick}>
                         <span className="text-link">
                             {t("Home")}
                         </span>
                     </Link>
                 </li>
                 <li className="menu-item nav-item menu-item-has-children dropdown mennucolumn_full-six">
-                    <Link href="#about" className="nav_link">
+                    <Link href="#about" className="nav_link" onClick={handleLinkClick}>
                         <span className="text-link">
                             {t("About us")}
                         </span>
                     </Link>
                 </li>
                 <li className="menu-item  menu-item-has-children dropdown  mennucolumn_ nav-item">
-                    <Link href="#offers" className="nav_link">
+                    <Link href="#offers" className="nav_link" onClick={handleLinkClick}>
                         <span className="text-link">
                             {t("Offers")}
                         </span>
                     </Link>
                 </li>
                 <li className="menu-item menu-item-has-children dropdown  nav-item">
-                    <Link href="#services" className="nav_link">
+                    <Link href="#services" className="nav_link" onClick={handleLinkClick}>
                         <span className="text-link">
                             {t("Services")}
                         </span>
                     </Link>
                 </li>
                 <li className="menu-item  menu-item-has-children dropdown  mennucolumn_  nav-item">
-                    <Link href="#process" className="nav_link">
+                    <Link href="#process" className="nav_link" onClick={handleLinkClick}>
                         <span className="text-link">
                            {t("Working process")}
                         </span>
                     </Link>
                 </li>
                 <li className="menu-item  menu-item-has-children dropdown  mennucolumn_two   nav-item">
-                    <Link href="/clients" className="nav_link">
+                    <Link href="#clients" className="nav_link" onClick={handleLinkClick}>
                         <span className="text-link">
                             {t("Our clients")}
                         </span>
                     </Link>
                 </li>
                 <li className="menu-item  menu-item-has-children dropdown  mennucolumn_  nav-item">
-                    <Link href="#news" className="nav_link">
+                    <Link href="#news" className="nav_link" onClick={handleLinkClick}>
                         <span className="text-link">
                             {t("News")}
                         </span>
