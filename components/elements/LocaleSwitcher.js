d30ef8c6fb1930f458d38bd753e2baca19d1c811
malchikprostoy <malchikprostoy5@gmail.com>
2024-05-23 12:04:53 +0600

    fix some bugs in switch-language

--- components/elements/LocaleSwitcher.js
+++ components/elements/LocaleSwitcher.js
@@ -1,28 +1,39 @@
-import {useRouter} from "next/router";
-import {useLocale} from "next-intl";
+import { useRouter } from "next/router";
+import { useLocale } from "next-intl";
+import { useEffect } from "react";
 
 export default function LocaleSwitcher() {
     const router = useRouter();
     const locale = useLocale();
     const locales = router.locales ?? [];
-    const handleLocaleSwitch = (locale) => {
-        router.push({pathname: router.pathname, query: router.query}, router.asPath, {locale});
+
+    const handleLocaleSwitch = async (newLocale) => {
+        // Сохранение текущей позиции прокрутки в sessionStorage
+        sessionStorage.setItem('scrollPosition', window.scrollY);
+
+        // Переход на новую локаль
+        await router.push({ pathname: router.pathname, query: router.query }, router.asPath, { locale: newLocale });
+
+        // Восстановление позиции прокрутки из sessionStorage
+        const scrollPosition = sessionStorage.getItem('scrollPosition');
+        if (scrollPosition) {
+            window.scrollTo(0, parseInt(scrollPosition, 10));
+            sessionStorage.removeItem('scrollPosition');
+        }
     };
+
     return (
         <div className="language">
             <a className="language-dropdown-active">
-                {locale.toUpperCase()} <i className="fi-rs-angle-small-down"/>
+                {locale.toUpperCase()} <i className="fi-rs-angle-small-down" />
             </a>
             <ul className="language-dropdown">
                 {locales.map((itemLocale) => (
-                    <li
-                        key={itemLocale}
-                        onClick={() => handleLocaleSwitch(itemLocale)}
-                    >
+                    <li key={itemLocale} onClick={() => handleLocaleSwitch(itemLocale)}>
                         {itemLocale.toUpperCase()}
                     </li>
                 ))}
             </ul>
         </div>
     );
-};
+}
